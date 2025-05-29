f74f459a2907a7d3e75c1e0a382fa7714964f10a
Chloe Lim <chloelim608@gmail.com>
2025-05-28 20:52:01 -0700

    fix: clean up index.tsx
    remove unnecessary comments in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -63,16 +63,3 @@ registerRoute({
     exact: true,
     name: 'models',
 });
-
-
-
-// Below are some imports you may want to use.
-//   See README.md for links to plugin development documentation.
-// import { SectionBox } from '@kinvolk/headlamp-plugin/lib/CommonComponents';
-// import { K8s } from '@kinvolk/headlamp-plugin/lib/K8s';
-// import { Typography } from '@mui/material';
-
-
-
-
-
