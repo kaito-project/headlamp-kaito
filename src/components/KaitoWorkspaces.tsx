4ca21e9306c38cf05e96279a0a73effc4991925c
Chloe Lim <chloelim608@gmail.com>
2025-06-03 10:30:42 -0700

    fix: add imports

--- src/components/KaitoWorkspaces.tsx
+++ src/components/KaitoWorkspaces.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import {
+  DetailsGrid,
+  NameValueTable,
+  SectionBox,
+} from '@kinvolk/headlamp-plugin/lib/components/common';
+import { useParams } from 'react-router-dom';
 
+const { name, namespace } = useParams<{ name: string; namespace: string }>();
 const KaitoWorkspaces: React.FC = () => {
   return (
     <div>
