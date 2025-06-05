412f39b7a4f3b8edccbdadcc2d9d686f75b4d8f5
Chloe Lim <chloelim608@gmail.com>
2025-06-05 10:53:47 -0700

    fix: fixed extraInfo and extraSection

--- src/components/KaitoWorkspacesDetail.tsx
+++ src/components/KaitoWorkspacesDetail.tsx
@@ -4,7 +4,8 @@ import {
   SectionBox,
 } from '@kinvolk/headlamp-plugin/lib/components/common';
 import { useParams } from 'react-router-dom';
-import { Workspace } from './resources/workspace';
+import { KaitoWorkspace, Workspace } from './resources/workspace';
+import KaitoWorkspacesList from './KaitoWorkspacesList';
 
 const StringArray = ({ items }: { items?: string[] }) => (items?.length ? items.join(', ') : '-');
 
@@ -17,82 +18,81 @@ export function WorkspaceDetail() {
       namespace={namespace}
       resourceType={Workspace}
       withEvents
-      // extraInfo={(item: KaitoWorkspacesList) =>
-      //   item && [
-      //     {
-      //       name: 'Tuning Method',
-      //       value: item.tuning?.method || '-',
-      //     },
-      //     {
-      //       name: 'Preset Name',
-      //       value: item.tuning?.preset?.name || item.inference?.preset?.name || '-',
-      //     },
-      //     {
-      //       name: 'Preset Image',
-      //       value:
-      //         item.tuning?.preset?.presetOptions?.image ||
-      //         item.inference?.preset?.presetOptions?.image ||
-      //         '-',
-      //     },
-      //     {
-      //       name: 'Config',
-      //       value: item.tuning?.config || item.inference?.config || '-',
-      //     },
-      //     {
-      //       name: 'Worker Nodes',
-      //       value: item.status?.workerNodes?.join(', ') || '-',
-      //     },
-      //   ]
-      // }
-      // extraSections={(item: KaitoWorkspace) =>
-      //   item && [
-      //     {
-      //       id: 'ResourceSpec',
-      //       section: item.resource && (
-      //         <SectionBox title="Resources">
-      //           <NameValueTable
-      //             rows={[
-      //               {
-      //                 name: 'Count',
-      //                 value: item.resource.count?.toString() || '-',
-      //               },
-      //               {
-      //                 name: 'Instance Type',
-      //                 value: item.resource.instanceType || '-',
-      //               },
-      //               {
-      //                 name: 'Preferred Nodes',
-      //                 value: StringArray({ items: item.resource.preferredNodes }),
-      //               },
-      //               {
-      //                 name: 'Label Selector (matchLabels)',
-      //                 value: item.resource.labelSelector?.matchLabels
-      //                   ? JSON.stringify(item.resource.labelSelector.matchLabels)
-      //                   : '-',
-      //               },
-      //             ]}
-      //           />
-      //         </SectionBox>
-      //       ),
-      //     },
-      //     {
-      //       id: 'Status',
-      //       section: item.status?.conditions && (
-      //         <SectionBox title="Status Conditions">
-      //           <NameValueTable
-      //             rows={item.status.conditions.map(c => ({
-      //               name: c.type,
-      //               value: `${c.status} ${c.reason ? `(${c.reason})` : ''} ${
-      //                 c.message ? `- ${c.message}` : ''
-      //               }`,
-      //             }))}
-      //           />
-      //         </SectionBox>
-      //       ),
-      //     },
-      //   ]
-      // }
+      extraInfo={(item: KaitoWorkspace) =>
+        item && [
+          {
+            name: 'Tuning Method',
+            value: item.tuning?.method || '-',
+          },
+          {
+            name: 'Preset Name',
+            value: item.tuning?.preset?.name || item.inference?.preset?.name || '-',
+          },
+          {
+            name: 'Preset Image',
+            value:
+              item.tuning?.preset?.presetOptions?.image ||
+              item.inference?.preset?.presetOptions?.image ||
+              '-',
+          },
+          {
+            name: 'Config',
+            value: item.tuning?.config || item.inference?.config || '-',
+          },
+          {
+            name: 'Worker Nodes',
+            value: item.status?.workerNodes?.join(', ') || '-',
+          },
+        ]
+      }
+      extraSections={(item: KaitoWorkspace) =>
+        item && [
+          {
+            id: 'ResourceSpec',
+            section: item.resource && (
+              <SectionBox title="Resources">
+                <NameValueTable
+                  rows={[
+                    {
+                      name: 'Count',
+                      value: item.resource.count?.toString() || '-',
+                    },
+                    {
+                      name: 'Instance Type',
+                      value: item.resource.instanceType || '-',
+                    },
+                    {
+                      name: 'Preferred Nodes',
+                      value: StringArray({ items: item.resource.preferredNodes }),
+                    },
+                    {
+                      name: 'Label Selector (matchLabels)',
+                      value: item.resource.labelSelector?.matchLabels
+                        ? JSON.stringify(item.resource.labelSelector.matchLabels)
+                        : '-',
+                    },
+                  ]}
+                />
+              </SectionBox>
+            ),
+          },
+          {
+            id: 'Status',
+            section: item.status?.conditions && (
+              <SectionBox title="Status Conditions">
+                <NameValueTable
+                  rows={item.status.conditions.map(c => ({
+                    name: c.type,
+                    value: `${c.status} ${c.reason ? `(${c.reason})` : ''} ${
+                      c.message ? `- ${c.message}` : ''
+                    }`,
+                  }))}
+                />
+              </SectionBox>
+            ),
+          },
+        ]
+      }
     />
   );
 }
-//detail page
