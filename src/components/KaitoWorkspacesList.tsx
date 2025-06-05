afb0bad58d5881a72567852d93e95b33b9238b84
Ernest Wong <chwong719@gmail.com>
2025-06-05 11:34:27 -0700

    feat: add condition status labels
    Signed-off-by: Ernest Wong <chwong719@gmail.com>

--- src/components/KaitoWorkspacesList.tsx
+++ src/components/KaitoWorkspacesList.tsx
@@ -1,9 +1,48 @@
+import { Icon } from '@iconify/react';
+import { LightTooltip, ResourceListView, StatusLabel } from '@kinvolk/headlamp-plugin/lib/CommonComponents';
+import { Box } from '@mui/material';
 import React from 'react';
-import { ResourceListView } from '@kinvolk/headlamp-plugin/lib/CommonComponents';
-import { DateLabel } from '@kinvolk/headlamp-plugin/lib/components/common';
 import { Workspace } from './resources/workspace';
 
 const KaitoWorkspacesList: React.FC = () => {
+  const makeConditionStatusLabel = (conditionType: string) => {
+    return (workspace: Workspace) => {
+      const condition = workspace.status?.conditions?.find(
+        (cond) => cond.type === conditionType
+      );
+      if (!condition) {
+        return (
+          <Box display="inline-block">
+            <StatusLabel status="">
+              N/A
+            </StatusLabel>
+          </Box>
+        );
+      }
+      const getConditionStatus = (condition) => {
+        if (condition.status === 'True') return 'success'; // success = green
+        if (condition.status === 'False') return 'error'; // error = red
+        return 'warning'; // warning = yellow
+      };
+      const status = getConditionStatus(condition);
+
+      return (
+        <Box display="inline">
+          <LightTooltip title={condition.message} placement="top">
+            <Box display="inline">
+              <StatusLabel status={getConditionStatus(condition)}>
+                {(status === 'warning' || status === 'error') && (
+                  <Icon aria-label="hidden" icon="mdi:alert-outline" width="1.2rem" height="1.2rem" />
+                )}
+                {condition.status === 'True' ? "Ready" : "Not Ready"}
+              </StatusLabel>
+            </Box>
+          </LightTooltip>
+        </Box >
+      );
+    };
+  }
+
   return (
     <ResourceListView
       title="Workspaces"
@@ -20,23 +59,26 @@ const KaitoWorkspacesList: React.FC = () => {
           id: 'resourceReady',
           label: 'Resource Ready',
           getValue: item => item.resourceReady,
+          render: makeConditionStatusLabel('ResourceReady'),
         },
         {
           id: 'inferenceReady',
           label: 'Inference Ready',
           getValue: item => item.inferenceReady,
+          render: makeConditionStatusLabel('InferenceReady'),
         },
         {
           id: 'jobStarted',
           label: 'Job Started',
           getValue: item => item.jobStarted,
+          render: makeConditionStatusLabel('JobStarted'),
         },
         {
           id: 'workspaceSucceeded',
           label: 'Workspace Succeeded',
           getValue: item => item.workspaceSucceeded,
+          render: makeConditionStatusLabel('WorkspaceSucceeded'),
         },
-
         'age',
       ]}
     />
