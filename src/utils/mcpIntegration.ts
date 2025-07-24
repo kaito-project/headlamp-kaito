d3b24260988c8c80ae3d8a85621981c3e7ebce1a
Chloe Lim <chloelim608@gmail.com>
2025-07-24 13:00:08 -0700

    fix lint mcpIntegration

--- src/utils/mcpIntegration.ts
+++ src/utils/mcpIntegration.ts
@@ -28,8 +28,8 @@ export interface MCPServerStatusEvent {
     transportType: string;
   }[];
 }
-
-export type MCPEventListener = (event: MCPServerStatusEvent) => void;
+  // eslint-disable-next-line no-unused-vars
+export type MCPEventListener = (_event: MCPServerStatusEvent) => void;
 
 export class MCPIntegration {
   private clientManager: MCPClientManager;
