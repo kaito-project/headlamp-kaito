eb2a46c9cce3dc2b131bfd348219fa7ff4d9081d
Chloe Lim <chloelim608@gmail.com>
2025-08-07 14:48:27 -0700

    remove unused variable

--- src/utils/skuUtils.ts
+++ src/utils/skuUtils.ts
@@ -67,7 +67,6 @@ function generateLabel(sku: string, gpuModel: string, gpuCount: number, gpuMemGB
     .replace('AMD ', '')
     .replace(' accelerators', '');
   
-  const totalMemory = gpuCount * gpuMemGB;
   const memoryDisplay = gpuCount > 1 ? `${gpuCount}x${gpuMemGB}GB` : `1x${gpuMemGB}GB`;
   
   if (provider === 'aws') {
